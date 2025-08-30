c0a50a8e73141b887888b0d60eb7f84db2fca87d
GuiCorbulin <guilherme.rcorbulin@gmail.com>
2025-08-30 12:01:20 -0300

    aulaexemplo31.py

--- aulaexemplo31.py
+++ aulaexemplo31.py
@@ -1,4 +1,16 @@
+print ("Nossa primeira calculadora") #o comando print escreve na tela um texto predefinido ou então o valor de uma variavel 
+numero01 = int(input("Insira o primeiro numero:"))
+numero02 = int(input("Insira o segundo numero:")) #COMANDO INPUT COLETA UM VALOR DIGITADO PELO USUARIO 
+resultadosoma = numero01 + numero02
 
+print(resultadosoma)
+
+# ----------------------------------------------------------------------------------------
+numero01 = int(input("Insira o primeiro numero:"))
+numero02 = int(input("Insira o segundo numero:")) #COMANDO INPUT COLETA UM VALOR DIGITADO PELO USUARIO 
+resultadosoma = numero01 - numero02
+
+print(resultadosoma) #UMA CALCULADORA PARA SUBTRAÇÃO   
 
 # ----------------------------------------------------------------------------------------
 numero01 = int(input("Insira o primeiro numero:"))
