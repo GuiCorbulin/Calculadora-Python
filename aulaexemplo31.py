2be2f148209b4f76f7edb5a66e62057b5a144555
GuiCorbulin <guilherme.rcorbulin@gmail.com>
2025-08-30 12:00:16 -0300

    Add files via upload

---
+++ aulaexemplo31.py
@@ -0,0 +1,8 @@
+
+
+# ----------------------------------------------------------------------------------------
+numero01 = int(input("Insira o primeiro numero:"))
+numero02 = int(input("Insira o segundo numero:")) #COMANDO INPUT COLETA UM VALOR DIGITADO PELO USUARIO 
+resultadosoma = numero01 / numero02
+
+print(resultadosoma) #UMA CALCULADORA PARA Divisão  
